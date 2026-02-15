2308ace99c2323d0ecbe3c0a291ad4b6c4c4c1b6
suraj kumar shah <153660663+SurajTEACH@users.noreply.github.com>
2026-02-15 16:47:55 +0530

    Change API base URL to production endpoint

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://product-app-64xm.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
