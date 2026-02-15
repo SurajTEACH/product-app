6d0353d32256aafa2ae8a170a2af7a8cb7132687
suraj kumar shah <153660663+SurajTEACH@users.noreply.github.com>
2026-02-15 16:51:04 +0530

    Update CORS origin to production URL

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ connectDB();
 const app = express();
 
 app.use(cors({
-      origin:'http://localhost:5173',
+      origin:'https://product-app-1-zzr5.onrender.com',
       credentials:true
 }));
 
